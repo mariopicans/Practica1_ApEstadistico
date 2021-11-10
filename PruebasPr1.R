5f20570cdac67d33b0bf154eb08a9b4fabb0c011
prmario <94065954+prmario@users.noreply.github.com>
2021-11-10 17:51:43 +0100

    Add files via upload

--- PruebasPr1.R
+++ PruebasPr1.R
@@ -52,3 +52,36 @@ caret::confusionMatrix(pred,obs)
 rf.caret<-train(Tipo~.,data=train,method="rf")
 mtry.class<-sqrt(ncol(train)-1)
 tuneGrid
+
+
+# Ejercicio 3
+#Apartado a)
+library(kernlab)
+set.seed(40)
+svm<-ksvm(Tipo~.,data=train)
+svm
+
+pred<-predict(svm,newdata=test)
+caret::confusionMatrix(pred,test$Tipo)
+
+
+#Apartado b)
+tune.grid<-expand.grid(sigma=c(0.01,0.05,0.1),C=c(0.5,1,10),error=NA)
+
+best.err <- Inf
+set.seed(40)
+for (i in 1:nrow(tune.grid)){
+  fit<-ksvm(Tipo~.,data=train[,],cross=10,C=tune.grid$C[i],kpar=list(tune.grid$sigma[i]))
+  fit.error<-fit@cross
+  tune.grid$error[i] <- fit.error
+  if (fit.error < best.err) {
+    final.model <- fit
+    best.err <- fit.error
+    best.tune <- tune.grid[i, ]
+  }
+}
+
+final.model
+
+pred2<-predict(final.model,newdata=test)
+caret::confusionMatrix(pred2,test$Tipo)
